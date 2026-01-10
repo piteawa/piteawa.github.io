a43b34978d556abda4e333aeb6ba882955fb49e1
guanxin <164321357+piteawa@users.noreply.github.com>
2026-01-10 19:35:22 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -24,9 +24,8 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: "寻隐轩",
 	subtitle: "只在此山中，云深不知处",
-	siteURL: "siteURL: https://piteawa.github.io/awa/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "siteURL: https://piteawa.github.io/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2026-01-10", // 站点开始运行日期，用于站点统计组件计算运行天数
-
 	timeZone: SITE_TIMEZONE,
 
 	lang: SITE_LANG,
