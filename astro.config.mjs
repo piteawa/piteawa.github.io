43f5a03c51c954a1b5e9f19bf1998fdb4eb2413c
guanxin <164321357+piteawa@users.noreply.github.com>
2026-01-10 19:53:53 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,7 +30,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	site: siteConfig.siteURL,
-	base: "/awa/",
+	base: "/",
 	trailingSlash: "always",
 
 	output: "static",
